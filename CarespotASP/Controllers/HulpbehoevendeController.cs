a601912799e126e5a2a405e3f2f12548b9b7f356
lucschmeits <luc.06@live.nl>
2017-05-11 12:55:44 +0200

    opdrachtensherm

---
+++ CarespotASP/Controllers/HulpbehoevendeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarespotASP.Controllers
+{
+    public class HulpbehoevendeController : Controller
+    {
+        // GET: Hulpbehoevende
+        public ActionResult Index()
+        {
+            return View("Opdrachten");
+        }
+    }
+}

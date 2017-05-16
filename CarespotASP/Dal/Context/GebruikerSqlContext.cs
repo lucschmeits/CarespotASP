341befb8cf6d6d2fb597961975836ae1c78b82bb
Lars <larslemkens@gmail.com>
2017-05-16 11:05:38 +0200

    bezig :)

--- CarespotASP/Dal/Context/GebruikerSqlContext.cs
+++ CarespotASP/Dal/Context/GebruikerSqlContext.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using CarespotASP.Dal.Interfaces;
+using CarespotASP.Models;
 
 namespace CarespotASP.Dal.Context
 {
-    public class GebruikerSqlContext
+    public class GebruikerSqlContext : IGebruiker
     {
+        public List<Gebruiker> GetAllGebruikers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Gebruiker GetGebruikerById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CreateGebruiker()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateGebruiker()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteGebruikerById(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

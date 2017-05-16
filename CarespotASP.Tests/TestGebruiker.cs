a6473b47b23d3ae68fd70cc6846c0d597909a222
Lars <larslemkens@gmail.com>
2017-05-16 13:20:07 +0200

    nog 2 testen voor gebruiker

---
+++ CarespotASP.Tests/TestGebruiker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using CarespotASP.Dal.Context;
+using CarespotASP.Dal.Repositorys;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarespotASP.Tests
+{
+    [TestClass]
+    public class TestGebruiker
+    {
+        [TestMethod]
+        public void GetAll()
+        {
+            GebruikerSqlContext gsc = new GebruikerSqlContext();
+            GebruikerRepository gr = new GebruikerRepository(gsc);
+
+            var Lijst = gr.GetAll();
+            Assert.IsTrue(Lijst.Count > 0);
+        }
+
+
+        [TestMethod]
+        public void GetById()
+        {
+            GebruikerSqlContext gsc = new GebruikerSqlContext();
+            GebruikerRepository gr = new GebruikerRepository(gsc);
+            int id = 1;
+            var User = gr.GetById(id);
+            Assert.AreEqual(id, User.Id);
+        }
+
+
+        [TestMethod]
+        public void Create()
+        {
+            GebruikerSqlContext gsc = new GebruikerSqlContext();
+            GebruikerRepository gr = new GebruikerRepository(gsc);
+
+            var User = gr.GetById(1);
+
+            int id = gr.Create(User);
+            var nieuweUser = gr.GetById(id);
+
+            Assert.AreEqual(id, nieuweUser.Id);
+
+        }
+
+        
+
+    }
+}
--- CarespotASP/Dal/Repositorys/GebruikerRepository.cs
+++ CarespotASP/Dal/Repositorys/GebruikerRepository.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using CarespotASP.Dal.Interfaces;
+using CarespotASP.Models;
 
 namespace CarespotASP.Dal.Repositorys
 {
     public class GebruikerRepository
     {
+        private IGebruiker _interface;
+        public GebruikerRepository(IGebruiker i)
+        {
+            _interface = i;
+        }
+
+        public int Create(Gebruiker obj)
+        {
+            return _interface.CreateGebruiker(obj);
+        }
+        public List<Gebruiker> GetAll()
+        {
+            return _interface.GetAllGebruikers();
+        }
+
+        public Gebruiker GetById(int id)
+        {
+            return _interface.GetGebruikerById(id);
+        }
+
+        public void Update(Gebruiker obj)
+        {
+            _interface.UpdateGebruiker(obj);
+        }
+
+        public void Delete(int id)
+        {
+            _interface.DeleteGebruikerById(id);
+        }
     }
 }

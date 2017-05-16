161b75e68f13a9b4a6bb8e465f343d103405d8f5
lucschmeits <luc.06@live.nl>
2017-05-16 12:27:29 +0200

    mer

---
+++ CarespotASP.Tests/TestChat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CarespotASP.Dal.Context;
+using CarespotASP.Dal.Repositorys;
+using CarespotASP.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarespotASP.Tests
+{
+    [TestClass]
+    public class TestChat
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var sql = new ChatSqlContext();
+            var repo = new ChatRepository(sql);
+            var g1 = new Gebruiker(1);
+            var g2 = new Gebruiker(2);
+
+            var chat = new Chat(g1, g2, DateTime.Now, "Hallo jongens");
+            repo.Create(chat);
+        }
+    }
+}

441827b33196077142551e6104aae92d4570d63a
lucschmeits <luc.06@live.nl>
2017-05-16 11:04:37 +0200

    connectionstring - context

---
+++ CarespotASP/Dal/Context/ChatSqlContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CarespotASP.Dal.Interfaces;
+using CarespotASP.Models;
+
+namespace CarespotASP.Dal.Context
+{
+    public class ChatSqlContext : IChat
+    {
+        public void Create(Chat chat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Chat> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Chat GetById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

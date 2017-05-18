0a1a655b386322f587e52a6f6b07ecb430f431d0
lucschmeits <luc.06@live.nl>
2017-05-18 12:45:55 +0200

    hulpverlener repo

---
+++ CarespotASP/Dal/Context/HulpverlenerSqlContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CarespotASP.Dal.Interfaces;
+using CarespotASP.Models;
+
+namespace CarespotASP.Dal.Context
+{
+    public class HulpverlenerSqlContext : IHulpverlener
+    {
+        public void Create(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Hulpverlener> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hulpverlener GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

8dd9e9f542f203daa8984c3b7863fa2053941b61
PeterSmallenbroek <PeterSmallenbroek@users.noreply.github.com>
2017-05-18 12:12:42 +0200

    Testje

---
+++ CarespotASP.Tests/TestReview.cs
@@ -0,0 +1,41 @@
+﻿using CarespotASP.Dal.Context;
+using CarespotASP.Dal.Repositorys;
+using CarespotASP.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarespotASP.Tests
+{
+    [TestClass]
+    public class TestReview
+    {
+        [TestMethod]
+        public void TestCreateReview()
+        {
+            var sql = new ReviewSqlContext();
+            var repo = new ReviewRepository(sql);
+
+            var review = new Review(1, 4, "Tralala", 8);
+            repo.CreateReview(review);
+        }
+
+        [TestMethod]
+        public void TestGetReviewById()
+        {
+            var sql = new ReviewSqlContext();
+            var repo = new ReviewRepository(sql);
+
+            Assert.AreEqual(1, repo.GetReviewById(2).AuteurId);
+        }
+
+        [TestMethod]
+        public void TestGetAllReview()
+        {
+            var sql = new ReviewSqlContext();
+            var repo = new ReviewRepository(sql);
+
+            var LstRvw = repo.GetAllReviews();
+
+            Assert.AreEqual(1, LstRvw[0].AuteurId);
+        }
+    }
+}
--- CarespotASP/Dal/Interfaces/IVrijwilliger.cs
+++ CarespotASP/Dal/Interfaces/IVrijwilliger.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using CarespotASP.Models;
 
 namespace CarespotASP.Dal.Interfaces
 {
-    interface IVrijwilliger
+    internal interface IVrijwilliger
     {
         List<Vrijwilliger> GetAllVrijwilligers();
+
         Vrijwilliger GetVrijwilligerById(int id);
+
         int CreateVrijwilliger();
+
         void UpdateVrijwilliger();
+
         void DeleteVrijwilligerById(int id);
     }
 }

03327d34fa2c80c30be60b56193af46cee6b8aa5
LanceGils <lance.vangils@student.fontys.nl>
2017-05-23 13:31:16 +0200

    gebruiker aanmaken window

---
+++ CarespotASP/Controllers/GebruikerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarespotASP.Controllers
+{
+    public class GebruikerController : Controller
+    {
+        // GET: Gebruiker
+        public ActionResult Index()
+        {
+            return View("NieuwGebruiker");
+        }
+    }
+}

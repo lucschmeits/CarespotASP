03d66ccd85dc30e68dde5ed20bad1fc57dbd1996
Lars <larslemkens@gmail.com>
2017-05-23 11:38:50 +0200

    Login gemaakt

--- CarespotASP/Controllers/LoginController.cs
+++ CarespotASP/Controllers/LoginController.cs
@@ -3,16 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CarespotASP.Dal.Repositorys;
+using CarespotASP.Models;
 
 namespace CarespotASP.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
-        public ActionResult Index(string email, string wachtwoord)
+        public ActionResult Index()
         {
-            return View("Login");
+            return View("~/Views/Login/Login.cshtml");
         }
+
+        //Post Login
+        [HttpPost]
+        public ActionResult LoginPost(FormCollection loginForm)
+        {
+            Gebruiker g = AuthRepository.CheckAuth(loginForm["email"], loginForm["wachtwoord"]);
+
+            if (g == null)
+            {
+
+                ViewBag.LoginResult = false;
+                return View("~/Views/Login/Login.cshtml");
+            }
+            else
+            {
+                return View("~/Views/Home/Index.cshtml");
+            }
+
+
+        }
+
+
 
         public ActionResult Keuze()
         {

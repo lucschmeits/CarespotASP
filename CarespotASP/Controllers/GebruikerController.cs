f4d1a07c6e90b0da41ef562d7c6e17e246a7cff5
Lars <larslemkens@gmail.com>
2017-06-06 14:13:30 +0200

    Gebruiker detail scherm gemaakt

--- CarespotASP/Controllers/GebruikerController.cs
+++ CarespotASP/Controllers/GebruikerController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CarespotASP.Dal.Context;
+using CarespotASP.Dal.Repositorys;
 
 namespace CarespotASP.Controllers
 {
@@ -12,6 +14,20 @@ namespace CarespotASP.Controllers
         public ActionResult Index()
         {
             return View("NieuwGebruiker");
+        }
+
+        //Get /Details/{id}
+        public ActionResult Details(int id)
+        {
+            GebruikerSqlContext gsc = new GebruikerSqlContext();
+            GebruikerRepository gr = new GebruikerRepository(gsc);
+
+            ReviewSqlContext rsc = new ReviewSqlContext();
+            ReviewRepository rr = new ReviewRepository(rsc);
+
+            ViewBag.SelectedUser = gr.GetById(id);
+            ViewBag.Reviews = rr.GetReviewByVrijwilligerId(id);
+            return View("~/Views/Gebruiker/Details.cshtml");
         }
     }
 }
